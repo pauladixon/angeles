55cdc1e144f7b78ea0af7850fbc687c65dcaa741
paula dixon <paulapdixon@gmail.com>
2021-03-24 13:11:35 -0700

    added directives and pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { TransformDirective } from './transform.directive';
+import { SafePipe } from './pipes/safe/safe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    TransformDirective,
+    SafePipe
   ],
   imports: [
     BrowserModule,
